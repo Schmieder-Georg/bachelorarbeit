97bdb82a3a0fdf5091f8c154a98ba9e46512b22b
Schmieder-Georg <schmieder.georg@web.de>
2022-05-09 01:27:39 +0200

    only 800 in results miussing

--- Plots/gm_relevanze/significanze_plots.py
+++ Plots/gm_relevanze/significanze_plots.py
@@ -10,12 +10,12 @@ def plot_all(data, x):
     print(data[13:26, 0])
     fig = plt.figure("MWZ", dpi=300)
     ax = fig.add_subplot()
-    ax.scatter(x, data[13:26, 0], label="S1")
-    ax.scatter(x, data[13:26, 1], label="M0")
-    ax.scatter(x, data[13:26, 2], label="M1")
-    ax.scatter(x, data[13:26, 3], label="T0")
-    ax.scatter(x, data[13:26, 4], label="T1")
-    ax.scatter(x, data[13:26, 5], label="T2")
+    ax.scatter(x, data[0:13, 0], label="S1")
+    ax.scatter(x, data[0:13, 1], label="M0")
+    ax.scatter(x, data[0:13, 2], label="M1")
+    ax.scatter(x, data[0:13, 3], label="T0")
+    ax.scatter(x, data[0:13, 4], label="T1")
+    ax.scatter(x, data[0:13, 5], label="T2")
 
     ax.legend()
     plt.xticks(x)
@@ -30,31 +30,31 @@ def plot_comp(data, x):
     operator = ["S1", "M0", "M1", "T0", "T1", "T2"]
     for i in range(6):
         fig = plt.figure("MWZ ", dpi=300)
-        plt.scatter(x, data[13:26, i], label=r'$1 \times \sigma_{GM}$')
-        plt.scatter(x, data[39:52, i], label=r'$2 \times \sigma_{GM}$')
-        plt.scatter(x, data[65:78, i], label=r'$0.5 \times \sigma_{GM}$')
+        plt.scatter(x, data[0:13, i], label=r'$1 \times \sigma_{GM}$')
+        plt.scatter(x, data[26:39, i], label=r'$2 \times \sigma_{GM}$')
+        plt.scatter(x, data[52:65, i], label=r'$0.5 \times \sigma_{GM}$')
         plt.legend()
         plt.xticks(x)
         plt.title(r'$M_{T}(WZ)$ cross-section comparison for '+operator[i])
         plt.xlabel("GM Model [GeV]")
         plt.ylabel(r'$S = \frac{Z_{GM}(0)}{Z_{EFT}(0)}$')
         plt.savefig(
-            "E:/Georg/Dokumente/Studium/bachelorarbeit/Plots/gm_relevanze/MTWZ_comparision_"+operator[i])
+            "E:/Georg/Dokumente/Studium/bachelorarbeit/Plots/gm_relevanze/MWZ_comparision_"+operator[i])
         plt.clf()
 
 
 def plot_op(data, x):
     operator = ["S1", "M0", "M1", "T0", "T1", "T2"]
     for i in range(6):
-        fig = plt.figure("MWZ ", dpi=300)
-        plt.scatter(x, data[0:13, i], label=operator[i], color='tab:blue')
+        fig = plt.figure("MTWZ ", dpi=300)
+        plt.scatter(x, data[13:26, i], label=operator[i], color='tab:blue')
         plt.legend()
         plt.xticks(x)
         plt.title(r'$M(WZ)$ for '+operator[i])
         plt.xlabel("GM Model [GeV]")
         plt.ylabel(r'$S = \frac{Z_{GM}(0)}{Z_{EFT}(0)}$')
         plt.savefig(
-            "E:/Georg/Dokumente/Studium/bachelorarbeit/Plots/gm_relevanze/MWZ_op_"+operator[i])
+            "E:/Georg/Dokumente/Studium/bachelorarbeit/Plots/gm_relevanze/MTWZ_op_"+operator[i])
         plt.clf()
 
 
@@ -66,7 +66,7 @@ def main():
         "E:/Georg/Dokumente/Studium/bachelorarbeit/Plots/gm_relevanze/GM_relevanze.csv", sep=",", header=None)
     data = file_pandas.to_numpy()
 
-    plot_op(data, x)
+    plot_all(data, x)
 
 
 if __name__ == "__main__":
